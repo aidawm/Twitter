4c009d42b5086e010fb3743ec5a040b9f8c7a3ec
sa9978 <sara.rhn78@gmail.com>
2021-12-05 19:16:53 +0330

    User's setters and getter is finished

--- src/main/java/org/ce/ap/server/User.java
+++ src/main/java/org/ce/ap/server/User.java
@@ -11,6 +11,14 @@ public class User {
     private final LocalDate registryDate;
     private String biography;
 
+    /**
+     *
+     * @param firstName is using for set firstName
+     * @param lastName is using for set lastName
+     * @param username is using for set username
+     * @param password is using for set password
+     * @param birthDate is using for set birthDate
+     */
     public User(String firstName, String lastName, String username, String password, LocalDate birthDate) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -21,5 +29,55 @@ public class User {
 
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public LocalDate getRegistryDate() {
+        return registryDate;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
 }
