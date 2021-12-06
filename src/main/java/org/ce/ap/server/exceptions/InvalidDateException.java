ad234b84ab6ae7296e40a736409593e914011379
sa9978 <sara.rhn78@gmail.com>
2021-12-06 21:17:00 +0330

    fix the last commit

---
+++ src/main/java/org/ce/ap/server/exceptions/InvalidDateException.java
@@ -0,0 +1,10 @@
+package main.java.org.ce.ap.server.exceptions;
+
+/**
+ * this exception handles the date exceptions
+ */
+public class InvalidDateException extends Exception{
+    public InvalidDateException(String message){
+        super(message);
+    }
+}
