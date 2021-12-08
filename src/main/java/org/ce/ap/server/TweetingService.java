6c9001c6be59264da0331e02f10039819b6ae3dd
sa9978 <sara.rhn78@gmail.com>
2021-12-08 21:26:50 +0330

    TweetingService is edited

--- src/main/java/org/ce/ap/server/TweetingService.java
+++ src/main/java/org/ce/ap/server/TweetingService.java
@@ -1,10 +1,14 @@
 package main.java.org.ce.ap.server;
 
+import main.java.org.ce.ap.server.exceptions.InvalidCharacterNumberException;
+
 public interface TweetingService {
+    void addNewTweet(User author,String text) throws InvalidCharacterNumberException;
+    void removeTweet(Tweet tweet);
     void like(Tweet tweet, User user);
     void unLike(Tweet tweet, User user);
-    void reply(Tweet tweet);
-    void removeReply(Tweet tweet);
-    void retweet(Tweet tweet);
-    void removeRetweet(Tweet tweet);
+    void reply(Tweet tweet, Tweet replyTweet);
+    void removeReply(Tweet tweet, Tweet replyTweet);
+    void retweet(Tweet tweet, User user, String text) throws InvalidCharacterNumberException;
+    void removeRetweet(Tweet tweet, Retweet retweet);
 }
